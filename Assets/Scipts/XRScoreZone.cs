731a80d47b346a67a8ed48c00361fbe4ac0e174e
Amer21-ta <83578098+Amer21-ta@users.noreply.github.com>
2026-02-16 22:50:54 +0100

    12

--- Assets/Scipts/XRScoreZone.cs
+++ Assets/Scipts/XRScoreZone.cs
@@ -1,19 +1,25 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;  // Wichtig für HashSet!
 
 public class XRScoreZone : MonoBehaviour
 {
     public int score = 0;
     public TextMeshProUGUI scoreText;
+    
+    // Das ist die ganze Zauberei - nur eine Zeile!
+    private HashSet<GameObject> scoredObjects = new HashSet<GameObject>();
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("XRObject"))
+        // Nur 3 Bedingungen:
+        // 1. Richtiger Tag? 
+        // 2. Noch nicht im HashSet?
+        if (other.CompareTag("XRObject") && !scoredObjects.Contains(other.gameObject))
         {
-            score += 10;
-            scoreText.text = "Score: " + score;
-
-            Destroy(other.gameObject);
+            scoredObjects.Add(other.gameObject);  // Objekt merken
+            score += 10;                          // Punkte geben
+            scoreText.text = "Score: " + score;   // Anzeige updaten
         }
     }
 }
